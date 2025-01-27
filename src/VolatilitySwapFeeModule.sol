82920e5388106e5163e4e19b1046cfb809fc4e5a
DevenMat <deven@valantis.xyz>
2025-01-26 18:03:53 -0800

    Update: volatility formula EMA

--- src/VolatilitySwapFeeModule.sol
+++ src/VolatilitySwapFeeModule.sol
@@ -2,30 +2,25 @@
 pragma solidity 0.8.24;
 
 import "@openzeppelin/contracts/utils/math/Math.sol";
-import {ISwapFeeModule} from "@valantis-core/src/swap-fee-modules/interfaces/ISwapFeeModule.sol";
-import {ISovereignOracle} from "@valantis-core/src/oracles/interfaces/ISovereignOracle.sol";
+import {SwapFeeModuleData, ISwapFeeModule} from "@valantis-core/swap-fee-modules/interfaces/ISwapFeeModule.sol";
 /**
  * @title Volatility Swap Fee Module.
  * @dev Volatility Swap Fee Module for Valantis Sovereign Pool.
  */
-contract VolatilitySwapFeeModule is ISwapFeeModule, ISovereignOracle {
+abstract contract VolatilitySwapFeeModule is ISwapFeeModule {
 
-//     struct SwapFeeModuleData {
-//     uint256 feeInBips;
-//     bytes internalContext;
-// }
-
-// Fee tiers
 uint256 public immutable FEE_MIN;
 uint256 public immutable FEE_MAX;
-// Volatility multiplier
+uint256 public immutable ALPHA;
 uint256 public immutable VOLATILITY_MULTIPLIER;
-// Volatility accumulator
+
 uint256 public volatilityAccumulator;
+uint256 public lastPrice;
 
 constructor(
     uint256 _feeMin,
     uint256 _feeMax,
+    uint256 _alpha,
     uint256 _volatilityMultiplier
 ) {
     require(_feeMin < _feeMax, "Invalid fee range");
@@ -33,8 +28,9 @@ constructor(
     
     FEE_MIN = _feeMin;
     FEE_MAX = _feeMax;
+    ALPHA = _alpha;
     VOLATILITY_MULTIPLIER = _volatilityMultiplier;
-}
+    }
 
 function getSwapFeeInBips(
         address _tokenIn,
@@ -43,24 +39,29 @@ function getSwapFeeInBips(
         address _user,
         bytes memory _swapFeeModuleContext
     ) external returns (SwapFeeModuleData memory swapFeeModuleData) {
-        volatilityAccumulator = 0;
-
-        swapFeeModuleData.feeInBips = FEE_MIN + volatilityAccumulator * VOLATILITY_MULTIPLIER;
+        // Calculate the fee in bips based on the volatility accumulator prior to the swap.
+        uint256 feeInBips = FEE_MIN + volatilityAccumulator * VOLATILITY_MULTIPLIER;
+        feeInBips = Math.min(feeInBips, FEE_MAX);
+        feeInBips = Math.max(feeInBips, FEE_MIN);
+        swapFeeModuleData.feeInBips = feeInBips;
         swapFeeModuleData.internalContext = abi.encode(0);
         return swapFeeModuleData;
     }
 
-function callbackOnSwapEnd(
+    function callbackOnSwapEnd(
         uint256 _effectiveFee,
-        int24 _spotPriceTick,
         uint256 _amountInUsed,
         uint256 _amountOut,
-        SwapFeeModuleData memory _swapFeeModuleData
+        bytes memory _swapFeeModuleContext
     ) external {
-        volatilityAccumulator += amountInMinusFee * fee;
-        volatilityAccumulator = Math.min(volatilityAccumulator, FEE_MAX);
-        volatilityAccumulator = Math.max(volatilityAccumulator, FEE_MIN);
+        // Update volatility accumulator using the final price, discluding the fee.
+        uint256 priceWithoutFee = _amountOut * 1e18 / (_amountInUsed * (1e18 - _effectiveFee));
+        uint256 priceRatio = Math.log2(priceWithoutFee / lastPrice);
+        volatilityAccumulator = EMA(priceRatio, ALPHA, volatilityAccumulator);
+        lastPrice = priceWithoutFee;
     }
 
-
+    function EMA(uint256 _value, uint256 _alpha, uint256 _accumulator) internal pure returns (uint256) {
+        return _value * _alpha + (1 - _alpha) * _accumulator;
+    }
 }
