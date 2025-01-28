af5e9ae6d112a4d0f147a673d89c6b1a9990c1e9
Deven Matthews <deven@valantis.xyz>
2025-01-28 12:00:12 -0800

    Add: Fee in bips

--- src/VolatilitySwapFeeModule.sol
+++ src/VolatilitySwapFeeModule.sol
@@ -7,26 +7,44 @@ import {SwapFeeModuleData, ISwapFeeModule} from "@valantis-core/swap-fee-modules
  * @title Volatility Swap Fee Module.
  * @dev Volatility Swap Fee Module for Valantis Sovereign Pool.
  */
-/// @notice Error thrown when a function restricted to the pool is called by another address
-error VSF__OnlyPool();
-/// @notice Error thrown when a function restricted to the owner is called by another address
-error VSF__OnlyOwner();
-/// @notice Error thrown when attempting to set pool to zero address
-error VSF__ZeroAddress();
-/// @notice Error thrown when attempting to set pool address more than once
-error VSF__PoolAlreadySet();
 
 contract VolatilitySwapFeeModule is ISwapFeeModule {
-    address public owner;
-    address public pool;
+    /**
+    *
+    *  CUSTOM ERRORS
+    *
+    */
+    error VolatilitySwapFeeModule__OnlyPool();
+    error VolatilitySwapFeeModule__OnlyOwner();
+    error VolatilitySwapFeeModule__ZeroAddress();
+    error VolatilitySwapFeeModule__PoolAlreadySet();
+
+    /**
+     *
+     *  CONSTANTS
+     *
+     */
     uint256 public immutable FEE_MIN;
     uint256 public immutable FEE_MAX;
     uint256 public immutable ALPHA;
     uint256 public immutable VOLATILITY_MULTIPLIER;
-    uint256 public immutable BIPS = 10_000;
+    uint256 public constant BIPS = 10_000;
+
+    /**
+     *
+     *  STATE VARIABLES
+     *
+     */
+    address public owner;
+    address public pool;
     uint256 public volatilityAccumulator;
     uint256 public lastPrice;
 
+    /**
+     *
+     *  CONSTRUCTOR
+     *
+     */
     constructor(
         uint256 _feeMin,
         uint256 _feeMax,
@@ -56,30 +74,25 @@ contract VolatilitySwapFeeModule is ISwapFeeModule {
     // @dev Modifier to ensure the function is called by the pool
     modifier onlyPool() {
         if (msg.sender != pool) {
-            revert VSF__OnlyPool();
+            revert VolatilitySwapFeeModule__OnlyPool();
         }
         _;
     }
     // @dev Modifier to ensure the function is called by the owner
     modifier onlyOwner() {
         if (msg.sender != owner) {
-            revert VSF__OnlyOwner();
+            revert VolatilitySwapFeeModule__OnlyOwner();
         }
         _;
     }
 
     /**
      *
-     *  EXTERNAL FUNCTIONS
+     *  VIEW FUNCTIONS
      *
      */
-    function setPool(address _pool) external onlyOwner {
-        if (_pool == address(0)) revert VSF__ZeroAddress();
-        // Pool can only be set once
-        if (pool != address(0)) revert VSF__PoolAlreadySet();
-        pool = _pool;
-    }
 
+    // @dev Get the volatility accumulator
     function getVolatilityOracle() external view returns (uint256) {
         return volatilityAccumulator;
     }
@@ -90,6 +103,7 @@ contract VolatilitySwapFeeModule is ISwapFeeModule {
         return Math.max(feeInBips, FEE_MIN);
     }
 
+    // @dev Get the swap fee in bips
     function getSwapFeeInBips(
             address,
             address,
@@ -113,43 +127,61 @@ contract VolatilitySwapFeeModule is ISwapFeeModule {
             return swapFeeModuleData;
         }
 
-        // @dev Callback function called by the pool after the swap has finished. ( Sovereign Pools )
-        // @param _effectiveFee The effective fee charged for the swap.
-        // @param _amountInUsed The amount of tokenIn used for the swap.
-        // @param _amountOut The amount of the tokenOut transferred to the user.
-        // @param _swapFeeModuleData The context data returned by getSwapFeeInBips. 
-        function callbackOnSwapEnd(
-            uint256 _effectiveFee,
-            uint256 _amountInUsed,
-            uint256 _amountOut,
-            SwapFeeModuleData memory
-        ) external onlyPool {
-            // Update volatility accumulator using the final price, discluding the fee.
-            uint256 priceWithoutFee = _amountOut * 1e18 / (_amountInUsed * (1e18 - _effectiveFee));
-            uint256 priceRatio = Math.log2(priceWithoutFee / lastPrice);
-            volatilityAccumulator = EMA(priceRatio, ALPHA, volatilityAccumulator);
-            lastPrice = priceWithoutFee;
-        }
+    /**
+     *
+     *  EXTERNAL FUNCTIONS
+     *
+     */
+    function setPool(address _pool) external onlyOwner {
+        if (_pool == address(0)) revert VolatilitySwapFeeModule__ZeroAddress();
+        // Pool can only be set once
+        if (pool != address(0)) revert VolatilitySwapFeeModule__PoolAlreadySet();
+        pool = _pool;
+    }
 
-        // @dev Exponential Moving Average (EMA) pure function.
-        // @param _value The value to calculate the EMA for.
-        // @param _alpha The alpha value for the EMA.
-        // @param _accumulator The accumulator value for the EMA.
-        // @return The EMA value.   
-        function EMA(uint256 _value, uint256 _alpha, uint256 _accumulator) internal pure returns (uint256) {
-            return (_value * _alpha + (1e6 - _alpha) * _accumulator) / 1e6;
-        }
+    // @dev Callback function called by the pool after the swap has finished. ( Sovereign Pools )
+    // @param _effectiveFee The effective fee charged for the swap.
+    // @param _amountInUsed The amount of tokenIn used for the swap.
+    // @param _amountOut The amount of the tokenOut transferred to the user.
+    // @param _swapFeeModuleData The context data returned by getSwapFeeInBips. 
+    function callbackOnSwapEnd(
+        uint256 _effectiveFee,
+        uint256 _amountInUsed,
+        uint256 _amountOut,
+        SwapFeeModuleData memory _swapFeeModuleData
+    ) external onlyPool {
+        // Update volatility accumulator using the final price, discluding the fee.
+        uint256 priceWithoutFee = _amountOut * 1e18 / (_amountInUsed * _swapFeeModuleData.feeInBips);
+        uint256 priceRatio = Math.log2(priceWithoutFee / lastPrice);
+        volatilityAccumulator = EMA(priceRatio, ALPHA, volatilityAccumulator);
+        lastPrice = priceWithoutFee;
+    }
 
-        // IGNORE THIS FUNCTION
-        // For compatibility with the Universal Pool Type (not implemented)
-        function callbackOnSwapEnd(
-            uint256,
-            int24,
-            uint256,
-            uint256,
-            SwapFeeModuleData memory
-        ) external pure {
-            // not implemented
-            return;
-        }
+    /**
+        *
+        *  INTERNAL FUNCTIONS
+        *
+        */
+
+    // @dev Exponential Moving Average (EMA) pure function.
+    // @param _value The value to calculate the EMA for.
+    // @param _alpha The alpha value for the EMA.
+    // @param _accumulator The accumulator value for the EMA.
+    // @return The EMA value.   
+    function EMA(uint256 _value, uint256 _alpha, uint256 _accumulator) internal pure returns (uint256) {
+        return (_value * _alpha + (1e6 - _alpha) * _accumulator) / 1e6;
+    }
+
+    // IGNORE THIS FUNCTION
+    // For compatibility with the Universal Pool Type (not implemented)
+    function callbackOnSwapEnd(
+        uint256,
+        int24,
+        uint256,
+        uint256,
+        SwapFeeModuleData memory
+    ) external pure {
+        // not implemented
+        return;
+    }
 }
